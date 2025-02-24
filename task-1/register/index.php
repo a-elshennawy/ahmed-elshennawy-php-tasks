0af8b6612087c62d5f72b6c848d9ba256cb68339
shennawy <ahmedshennawy997@gmail.com>
2025-02-24 21:45:27 +0200

    redister design adjustments

--- task-1/register/index.php
+++ task-1/register/index.php
@@ -21,14 +21,14 @@ if (isset($_POST['regBtn'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap.min.css">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="register.css">
     <title>Register</title>
 </head>
 
 <body>
     <div class="container">
-        <h1>Sign up Form</h1>
         <form method="POST" action="" class="regForm row">
+            <h1>Sign up Form</h1>
             <div class="inputgroup col-10">
                 <input type="text" name="userName" placeholder="Enter your username" required>
             </div>
@@ -41,15 +41,15 @@ if (isset($_POST['regBtn'])) {
             <button type="submit" name="regBtn" class="regBtn col-3">
                 sign up
             </button>
+
+            <?php if ($regSuccessful): ?>
+                <p>registration successful</p>
+                <p>
+                    Welcome to the website, <?= $userName ?>
+                </p>
+            <?php endif; ?>
         </form>
     </div>
-
-    <?php if ($regSuccessful): ?>
-        <h2>registration successful</h2>
-        <h2>
-            Welcome to the website, <?= $userName ?>
-        </h2>
-    <?php endif; ?>
 
     <script src="bootstrap.bundle.min.js"></script>
 </body>
