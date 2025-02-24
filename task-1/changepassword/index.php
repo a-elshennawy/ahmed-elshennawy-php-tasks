fcecbd2c6368f7a5b5176941e08b5909f057f84e
shennawy <ahmedshennawy997@gmail.com>
2025-02-24 22:12:54 +0200

    password change done

--- task-1/changepassword/index.php
+++ task-1/changepassword/index.php
@@ -1,16 +1,16 @@
 <?php
-
 $currPassword = "@#ShMr997!";
-
 $matchCurrentpass = true;
 $matchnewpass = true;
 $passchanged = false;
+$revealPass = false;
 
 if (isset($_POST['changeBtn'])) {
     if ($_POST['currPassword'] == $currPassword) {
         if ($_POST['newPassword'] == $_POST['confirmNewpassword']) {
             $passchanged = true;
             $currPassword = $_POST['newPassword'];
+            $revealPass = true;
         } else {
             $matchnewpass = false;
         }
@@ -18,6 +18,7 @@ if (isset($_POST['changeBtn'])) {
         $matchCurrentpass = false;
     }
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -27,40 +28,46 @@ if (isset($_POST['changeBtn'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap.min.css">
-    <link rel="stylesheet" href="main.css">
-    <title>change password</title>
+    <link rel="stylesheet" href="passwordchange.css">
+    <title>Change Password</title>
 </head>
 
 <body>
     <div class="container">
-        <h1>change password</h1>
         <form method="POST" action="" class="changepasswordFrom row">
-            <div class="inputgroup col-10">
-                <input type="password" name="currPassword" placeholder="enter your curent password" required>
+            <h1>Change Password</h1>
+            <div class="inputgroup col-12">
+                <input type="password" name="currPassword" placeholder="Enter your current password" required>
             </div>
-            <div class="inputgroup col-10">
-                <input type="password" name="newPassword" placeholder="choose new password" required>
+            <div class="inputgroup col-12">
+                <input type="password" name="newPassword" placeholder="Choose new password" required>
             </div>
-            <div class="inputgroup col-10">
-                <input type="password" name="confirmNewpassword" placeholder="confirm your new password" required>
+            <div class="inputgroup col-12">
+                <input type="password" name="confirmNewpassword" placeholder="Confirm your new password" required>
             </div>
-            <button type="submit" name="changeBtn" class="changeBtn col-3">
-                confirm
+            <button type="submit" name="changeBtn" class="changeBtn col-2">
+                Confirm
             </button>
+
+            <?php if ($matchCurrentpass == false) : ?>
+                <p class="incorrectCRP">Incorrect current password</p>
+            <?php endif; ?>
+
+            <?php if ($matchnewpass == false): ?>
+                <p class="pMismatch">Password mismatch</p>
+            <?php endif; ?>
+
+            <?php if ($passchanged): ?>
+                <p class="doneMSG">Password changed successfully</p>
+            <?php endif; ?>
+
+            <?php if ($revealPass): ?>
+                <p class="currpass">Current password: <?= htmlspecialchars($currPassword) ?> </p>
+            <?php endif; ?>
+
         </form>
+
     </div>
-
-    <?php if ($matchCurrentpass == false) : ?>
-        <h1>inncorrect current password</h1>
-    <?php endif; ?>
-
-    <?php if ($matchnewpass == false): ?>
-        <h1>password mismatch</h1>
-    <?php endif; ?>
-
-    <?php if ($passchanged): ?>
-        <h1>password changed successfully</h1>
-    <?php endif; ?>
 
     <script src="bootstrap.bundle.min.js"></script>
 </body>
