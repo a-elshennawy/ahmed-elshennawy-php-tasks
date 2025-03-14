45391b2e536e30e3bc2871650584e2d51b7bac06
Shennawy <ahmedshennawy997@gmail.com>
2025-03-15 00:30:56 +0200

    customer list style

--- task-2/index.php
+++ task-2/index.php
@@ -8,7 +8,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- libraries -->
   <link rel="stylesheet" href="css/Vendors/bootstrap.min.css">
-  <link rel="stylesheet" href="css/Vendors/all.min.css">
   <!-- customized stylesheet -->
   <link rel="stylesheet" href="css/main.css">
   <!-- title -->
@@ -46,7 +45,47 @@
     </div>
   </section>
 
-
+  <!-- customers list -->
+  <section>
+    <div class="container-fluid">
+      <table class="customersList">
+        <thead>
+          <td>id</td>
+          <td>name</td>
+          <td>actions</td>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>ahmed</td>
+            <td>
+              <a href="">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye">
+                  <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+                  <circle cx="12" cy="12" r="3" />
+                </svg>
+              </a>
+              <a href="">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-pen">
+                  <path d="M11.5 15H7a4 4 0 0 0-4 4v2" />
+                  <path d="M21.378 16.626a1 1 0 0 0-3.004-3.004l-4.01 4.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z" />
+                  <circle cx="10" cy="7" r="4" />
+                </svg>
+              </a>
+              <a href="">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2">
+                  <path d="M3 6h18" />
+                  <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                  <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                  <line x1="10" x2="10" y1="11" y2="17" />
+                  <line x1="14" x2="14" y1="11" y2="17" />
+                </svg>
+              </a>
+            </td>
+        </tbody>
+      </table>
+    </div>
+  </section>
 
 
   <!-- script -->
