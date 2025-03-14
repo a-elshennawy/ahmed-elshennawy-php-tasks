ff36e8266c10849d10a90af99cb602f558c0c20a
Shennawy <ahmedshennawy997@gmail.com>
2025-03-14 19:47:15 +0200

    initialize task-2

---
+++ task-2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- libraries -->
+     <link rel="stylesheet" href="css/Vendors/bootstrap.min.css">
+     <link rel="stylesheet" href="css/Vendors/all.min.css">
+     <!-- customized stylesheet -->
+    <link rel="stylesheet" href="css/main.css">
+    <!-- title -->
+    <title>CRUD PHP</title>
+</head>
+<body>
+    
+
+
+
+
+<!-- script -->
+ <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
