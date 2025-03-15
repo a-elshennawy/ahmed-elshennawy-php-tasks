ecf0d368dc6d6c1ac683945b42c66d281abefaac
Shennawy <ahmedshennawy997@gmail.com>
2025-03-15 04:48:55 +0200

    adding a count to show last in first with no.1 & ordering view

--- task-2/index.php
+++ task-2/index.php
@@ -1,5 +1,5 @@
 <?php
-
+$count = 1;
 // connecting our database
 $host = "localhost";
 $user = "root";
@@ -33,7 +33,7 @@ if (isset($_POST['send'])) {
 }
 
 // read products 
-$selectProds = "SELECT * FROM `product_with_categories`";
+$selectProds = "SELECT * FROM `product_with_categories` ORDER BY id DESC";
 $allProds = mysqli_query($connect, $selectProds);
 
 // 
@@ -105,24 +105,24 @@ $allProds = mysqli_query($connect, $selectProds);
         <?php foreach ($allProds as $item): ?>
           <tbody>
             <tr>
-              <td><?= $item['id'] ?></td>
+              <td><?= $count++ ?></td>
               <td><?= $item['ProductName'] ?></td>
               <td>
                 <a href="">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#00a0fd" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye">
                     <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
                     <circle cx="12" cy="12" r="3" />
                   </svg>
                 </a>
                 <a href="">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-pen">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fdd700" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-pen">
                     <path d="M11.5 15H7a4 4 0 0 0-4 4v2" />
                     <path d="M21.378 16.626a1 1 0 0 0-3.004-3.004l-4.01 4.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z" />
                     <circle cx="10" cy="7" r="4" />
                   </svg>
                 </a>
                 <a href="">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fd0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2">
                     <path d="M3 6h18" />
                     <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
                     <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
