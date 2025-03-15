398a79449a603ce4ef7485e7dd0ff14929f091ef
Shennawy <ahmedshennawy997@gmail.com>
2025-03-15 02:52:10 +0200

    database connection & error handeling using try/catch

--- task-2/index.php
+++ task-2/index.php
@@ -1,4 +1,20 @@
-<?php ?>
+<?php
+
+// connecting our database
+$host = "localhost";
+$user = "root";
+$password = "";
+$databaseName = "shop";
+
+
+try {
+  $connect = mysqli_connect($host, $user, $password, $databaseName);
+} catch (Exception $e) {
+  echo $e->getMessage();
+}
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
