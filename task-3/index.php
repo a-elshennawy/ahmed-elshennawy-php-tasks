77c0e32d2fad087a448fa053448879594bc0a81a
Shennawy <ahmedshennawy997@gmail.com>
2025-03-21 15:05:43 +0200

    shared + index structure

---
+++ task-3/index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+?>
+
+<!-- importing head -->
+<?php include_once './shared/head.php' ?>
+
+<!-- importing navbar -->
+<?php include_once './shared/nav.php' ?>
+
+<section>
+  <div class="container">
+    <h1>home page :)</h1>
+  </div>
+</section>
+
+<!-- importing script -->
+<?php include_once './shared/script.php' ?>
---
+++ task-3/shared/nav.php
@@ -0,0 +1,53 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <!-- home (database status) -->
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <!-- customers -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        customers
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">add</a></li>
+                        <li><a class="dropdown-item" href="#">list</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">no orders list</a></li>
+                    </ul>
+                </li>
+                <!-- orderes -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        orders
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">add</a></li>
+                        <li><a class="dropdown-item" href="#">list</a></li>
+                    </ul>
+                </li>
+                <!-- products -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        products
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">add</a></li>
+                        <li><a class="dropdown-item" href="#">list</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">not ordered list</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+
+        </div>
+    </div>
+</nav>
