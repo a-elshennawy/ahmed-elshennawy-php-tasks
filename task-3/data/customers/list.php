70530792fa2c77822f7d2183d4052fea225a83f5
Shennawy <ahmedshennawy997@gmail.com>
2025-03-21 16:32:40 +0200

    view single customer

--- task-3/data/customers/list.php
+++ task-3/data/customers/list.php
@@ -4,6 +4,14 @@ include_once '../../env/database.php';
 
 $selectCustomers = "SELECT * FROM `customers` ORDER BY id DESC";
 $allCustomers = mysqli_query($connect, $selectCustomers);
+
+if (isset($_GET['view'])) {
+    $id = $_GET['view'];
+    $selectOneCustomer = "SELECT * FROM `customers` WHERE id = $id";
+    $selectOneCustomerItem = mysqli_query($connect, $selectOneCustomer);
+
+    $oneCustomer = mysqli_fetch_assoc($selectOneCustomerItem);
+}
 ?>
 
 <!-- importing head -->
@@ -11,6 +19,7 @@ $allCustomers = mysqli_query($connect, $selectCustomers);
 
 <!-- importing navbar -->
 <?php include_once '../../shared/nav.php' ?>
+
 <!-- customers list -->
 <section>
     <div class="container">
@@ -27,14 +36,14 @@ $allCustomers = mysqli_query($connect, $selectCustomers);
                         <td><?= $customers['name'] ?></td>
                         <td>
                             <!-- view -->
-                            <a href="">
+                            <a href="http://localhost/instant-php/task-3/data/customers/list.php?view= <?= $customers['id'] ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#00a0fd" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye">
                                     <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
                                     <circle cx="12" cy="12" r="3" />
                                 </svg>
                             </a>
                             <!-- edit -->
-                            <a href="">
+                            <a href="http://localhost/instant-php/task-3/data/customers/add.php?edit=">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fdd700" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-pen">
                                     <path d="M11.5 15H7a4 4 0 0 0-4 4v2" />
                                     <path d="M21.378 16.626a1 1 0 0 0-3.004-3.004l-4.01 4.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z" />
@@ -42,7 +51,7 @@ $allCustomers = mysqli_query($connect, $selectCustomers);
                                 </svg>
                             </a>
                             <!-- delete -->
-                            <a href="">
+                            <a href="http://localhost/instant-php/task-3/data/customers/list.php?delete=">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fd0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2">
                                     <path d="M3 6h18" />
                                     <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
@@ -58,3 +67,26 @@ $allCustomers = mysqli_query($connect, $selectCustomers);
         </table>
     </div>
 </section>
+
+<!-- customer modal -->
+<?php if (isset($_GET['view'])): ?>
+    <div class="custModal">
+        <div class="modalcontent">
+            <h4>customer details
+                <a class="float-end" href="http://localhost/instant-php/task-3/data/customers/list.php">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x">
+                        <path d="M18 6 6 18" />
+                        <path d="m6 6 12 12" />
+                    </svg>
+                </a>
+            </h4>
+            <h6>
+                <strong>name :</strong> <?= $oneCustomer['name']  ?>
+                <hr>
+                <strong>phone : </strong> <?= $oneCustomer['phone'] ?>
+                <hr>
+                <strong>street address : </strong> <?= $oneCustomer['address'] ?>
+            </h6>
+        </div>
+    </div>
+<?php endif; ?>
