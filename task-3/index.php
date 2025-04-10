374d67b6bf7cc7570fdc4935e0e551abf60989bb
shennawy <ahmedshennawy997@gmail.com>
2025-04-10 19:59:33 +0200

    test comm

--- task-3/index.php
+++ task-3/index.php
@@ -19,15 +19,15 @@ $allCounters = mysqli_fetch_assoc($counters);
     <h2 class="homeHeader">current status</h2>
     <div class="status row">
       <a href="http://localhost/instant-php/task-3/data/customers/list.php" class="count col-3">
-        <h5 class="countTitle">our total customers</h5>
+        <h5 class="countTitle">total customers</h5>
         <h1 class="countValue"><?= $allCounters['total_customers'] ?></h1>
       </a>
       <a href="http://localhost/instant-php/task-3/data/products/list.php" class="count col-3">
-        <h5 class="countTitle">our total products</h5>
+        <h5 class="countTitle">total products</h5>
         <h1 class="countValue"><?= $allCounters['total_products'] ?></h1>
       </a>
       <a href="http://localhost/instant-php/task-3/data/orders/list.php" class="count col-3">
-        <h5 class="countTitle">our total orders</h5>
+        <h5 class="countTitle">total orders</h5>
         <h1 class="countValue"><?= $allCounters['total_orders'] ?></h1>
       </a>
       <a href="http://localhost/instant-php/task-3/data/customers/withoutorders.php" class="count col-3">
